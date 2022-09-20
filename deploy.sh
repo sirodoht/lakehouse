9aed5013b533302ed125a0d9401d007f79ecf8e4
Theodore Keloglou <zf@sirodoht.com>
2022-09-21 00:43:22 +0100

    change domain name on deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -x
 git push origin master
 
 # overwrite nginx config
-scp lakehouse.sirodoht.com.conf root@lakehouse.wiki:/etc/nginx/sites-available/
+scp lakehouse.wiki.conf root@lakehouse.wiki:/etc/nginx/sites-available/
 
 # pull and reload on server
 ssh root@lakehouse.wiki 'cd /opt/apps/lakehouse \

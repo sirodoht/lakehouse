fd812ba7b6dbc9f5822f01e2e544a6e76290991f
Theodore Keloglou <zf@sirodoht.com>
2022-09-22 00:39:21 +0100

    add signup template

--- user/user_page.go
+++ user/user_page.go
@@ -16,7 +16,7 @@ func NewPage() *Page {
 	return &Page{}
 }
 
-func (page *Page) Render(w http.ResponseWriter, r *http.Request) {
+func (page *Page) RenderLogin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	t, err := template.ParseFiles("templates/layout.html", "templates/login.html")
 	if err != nil {
@@ -32,7 +32,38 @@ func (page *Page) Render(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (page *Page) Form(w http.ResponseWriter, r *http.Request) {
+func (page *Page) CreateSession(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	type ReqBody struct {
+		Username string
+		Password string
+	}
+	rb := &ReqBody{
+		Username: username,
+		Password: password,
+	}
+	fmt.Printf("%+v", rb)
+}
+
+func (page *Page) RenderNew(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t, err := template.ParseFiles("templates/layout.html", "templates/signup.html")
+	if err != nil {
+		page.logger.With(
+			zap.Error(err),
+		).Error("cannot compile signup template")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (page *Page) SaveNew(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 

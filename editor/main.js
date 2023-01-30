15109282b857aa42c01774189b9690733bd1d0ec
Theodore Keloglou <zf@sirodoht.com>
2023-01-30 21:24:11 +0000

    add editor js app

---
+++ editor/main.js
@@ -0,0 +1,25 @@
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+import Collaboration from '@tiptap/extension-collaboration'
+import { HocuspocusProvider } from '@hocuspocus/provider'
+
+// Set up the Hocuspocus WebSocket provider
+const provider = new HocuspocusProvider({
+  url: 'ws://127.0.0.1:8001',
+  name: 'example-document',
+})
+
+const editor = new Editor({
+  element: document.querySelector(".element"),
+  content: "<p>Hello World!</p>",
+  extensions: [
+    StarterKit.configure({
+      // The Collaboration extension comes with its own history handling
+      history: false,
+    }),
+    // Register the document with Tiptap
+    Collaboration.configure({
+      document: provider.document,
+    }),
+  ],
+})
---
+++ editor/rollup.config.js
@@ -0,0 +1,10 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "main.js",
+  output: {
+    file: "../static/bundle.js",
+    format: "iife",
+  },
+  plugins: [nodeResolve()],
+};

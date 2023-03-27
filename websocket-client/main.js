ec13fdf4307d56a4a0e919c8aeeabbd62925be41
Theodore Keloglou <zf@sirodoht.com>
2023-03-27 21:06:18 +0100

    add hocuspocus on main js

--- websocket-client/main.js
+++ websocket-client/main.js
@@ -1,17 +1,25 @@
 import { Editor } from "@tiptap/core";
 import StarterKit from "@tiptap/starter-kit";
 import Collaboration from "@tiptap/extension-collaboration";
+// import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
+import * as Y from 'yjs'
 import { HocuspocusProvider } from "@hocuspocus/provider";
 
-// Set up the Hocuspocus WebSocket provider
+
+console.log("DOCUMENT_NAME:", DOCUMENT_NAME);
+console.log("DOCUMENT_BODY:", DOCUMENT_BODY);
+
+// websocket provider
 const provider = new HocuspocusProvider({
   url: "ws://127.0.0.1:8001",
-  name: "example-document",
+  name: DOCUMENT_NAME,
 });
 
+// instantiate tiptap editor
 new Editor({
+  // element: document.querySelector("#id_body"),
   element: document.querySelector(".element"),
-  content: "<p>Hello World!</p>",
+  // content: DOCUMENT_BODY,
   extensions: [
     StarterKit.configure({
       // The Collaboration extension comes with its own history handling
@@ -21,5 +29,12 @@ new Editor({
     Collaboration.configure({
       document: provider.document,
     }),
+    // CollaborationCursor.configure({
+    //   provider: provider,
+    //   user: {
+    //     name: "Cyndi Lauper",
+    //     color: "#f783ac",
+    //   },
+    // }),
   ],
 });

39461537985a9ab84ee4315dc2c842497266e6a8
Theodore Keloglou <zf@sirodoht.com>
2023-03-27 21:09:00 +0100

    fix lint no undef on main js

--- websocket-client/main.js
+++ websocket-client/main.js
@@ -2,17 +2,12 @@ import { Editor } from "@tiptap/core";
 import StarterKit from "@tiptap/starter-kit";
 import Collaboration from "@tiptap/extension-collaboration";
 // import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
-import * as Y from 'yjs'
 import { HocuspocusProvider } from "@hocuspocus/provider";
-
-
-console.log("DOCUMENT_NAME:", DOCUMENT_NAME);
-console.log("DOCUMENT_BODY:", DOCUMENT_BODY);
 
 // websocket provider
 const provider = new HocuspocusProvider({
   url: "ws://127.0.0.1:8001",
-  name: DOCUMENT_NAME,
+  name: DOCUMENT_NAME, // eslint-disable-line no-undef
 });
 
 // instantiate tiptap editor
